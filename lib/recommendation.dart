1db4c39117007ad9a3a2c8c48cf2f8d65f1cd4cc
maggieding317 <61335351+maggieding317@users.noreply.github.com>
2020-03-13 17:17:04 -0700

    updates

--- lib/recommendation.dart
+++ lib/recommendation.dart
@@ -4,14 +4,40 @@ class Recommendataion {
   var food_map = {
     "egg" : {
       "id" : "egg",
+      "name" : "egg",
+      "image" : "https://lh3.googleusercontent.com/proxy/wtqY1ThjGPtYU6fzx_pJ0rPyGF-svFNEyhegBA0f3mrjeFst_yxW8Idct3Ocpk29HeqT5rLdmcNR_GaIPWL1j1yt2_kfLgM8v4uz_7bMAm9i7DKg4YANdNwTzEcecSrc86m88kL1bOyD48GR-_2uYFJlnVZ6Qm4",
+    },
+    "bread":{
+      "id":"bread",
       "name" : "Bread",
       "image" : "https://lh3.googleusercontent.com/proxy/wtqY1ThjGPtYU6fzx_pJ0rPyGF-svFNEyhegBA0f3mrjeFst_yxW8Idct3Ocpk29HeqT5rLdmcNR_GaIPWL1j1yt2_kfLgM8v4uz_7bMAm9i7DKg4YANdNwTzEcecSrc86m88kL1bOyD48GR-_2uYFJlnVZ6Qm4",
     },
-    "pork" : {
-      "id" : "pork",
-      "name" : "Pork",
-      "image" : "https://lh3.googleusercontent.com/proxy/wtqY1ThjGPtYU6fzx_pJ0rPyGF-svFNEyhegBA0f3mrjeFst_yxW8Idct3Ocpk29HeqT5rLdmcNR_GaIPWL1j1yt2_kfLgM8v4uz_7bMAm9i7DKg4YANdNwTzEcecSrc86m88kL1bOyD48GR-_2uYFJlnVZ6Qm4",
+    "milk":{
+      "id":"milk",
+      "name" : "milk",
+      "image" : "https://cdn.iconscout.com/icon/premium/png-256-thumb/milk-1637594-1387047.png",
+    },
+    "bacon":{
+      "id":"bacon",
+      "name" : "bacon",
+      "image" : "https://cdn.iconscout.com/icon/premium/png-256-thumb/bacon-23-565349.png",
+    },
+    "sausage":{
+      "id":"sausage",
+      "name" : "sausage",
+      "image" : "https://cdn.iconscout.com/icon/premium/png-256-thumb/sausage-176-1051414.png",
+    },
+    "pork":{
+      "id":"sausage",
+      "name" : "sausage",
+      "image" : "https://cdn.iconscout.com/icon/premium/png-256-thumb/sausage-176-1051414.png",
+    },
+    "chicken":{
+      "id":"chicken",
+      "name" : "chicken",
+      "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRfM5V0hpPJGa9GVkIBVJX2m-dDP0qY-EhcJdyZo9weoWkjm-qE",
     }
+
   };
 
   var recommendation_options = {
